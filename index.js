13e69ffbdad81b2264acb72a8a18b375372ab1cb
KostyantynVegas <41579834+KostyantynVegas@users.noreply.github.com>
2018-07-23 16:46:18 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//conect library discord.js and client.
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+console.log("BotWorks");//наличие багов.
+
+//bot register.
+bot.login("NDcwMjU2MTc5NzgwNjQ4OTcx.DjYCwg.S-zss2_7QtOhxkWeXiitpnHynJc");
