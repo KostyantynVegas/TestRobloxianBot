520fde42fb49d7fbfd902bd13e7128840a8ba154
KostyantynVegas <41579834+KostyantynVegas@users.noreply.github.com>
2018-07-23 22:49:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ console.log("BotWorks");//наличие багов.
 
 //bot register.
 bot.login("NDcwMjU2MTc5NzgwNjQ4OTcx.DjYCwg.S-zss2_7QtOhxkWeXiitpnHynJc");
-bot.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_KEYFORROBLOX);
