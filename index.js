2ac881ea70157b52a2c7af329e63a7402ef36dc8
KostyantynVegas <41579834+KostyantynVegas@users.noreply.github.com>
2018-07-24 14:10:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ bot.on('message', message => {
 
 //answered.
        if (answered == false) {
-       AuthorMessage = message.author;
+       AuthorMessage = msg;
        if (AuthorMessage == Answers) {
        message.reply('Ты выиграл! :grinning:');
        }
